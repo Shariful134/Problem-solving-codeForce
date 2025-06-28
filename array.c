dba34feb27b408b3034c6d7a8c0ce462a31cc4fb
Shariful134 <shariful32213@gmail.com>
2025-06-28 18:07:33 +0600

    Insert a value in an array

--- array.c
+++ array.c
@@ -1,18 +1,26 @@
 #include<stdio.h>
 int main()
 {
-   int N;
-   scanf("%d", &N);
-   int A[N];
-   for(int i=0; i<N; i++){
-    scanf("%d", &A[i]);
+   int n;
+   scanf("%d", &n);
+   int a[n+1];
+  
+   for(int i=0; i<n; i++){
+    scanf("%d", &a[i]);
+   }
+   int idx, value;
+   scanf("%d %d", &idx, &value);
+
+   for(int i=n; i >= idx+1; i--){
+    a[i]= a[i-1];
+   }
+   a[idx]= value;
+   for(int i=0; i<n; i++){
+    printf("%d ",a[i]);
    }
 
-   for(int i=N-1; i>=0; i--){
-   if(i % 2 == 1){
-     printf("%d ", A[i]);
-   }
-  }
+
    
+
 return 0;
 }
