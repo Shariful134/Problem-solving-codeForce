7da2a4cb31a584094adb3935a344af2f052e4d19
Shariful134 <shariful32213@gmail.com>
2025-06-28 14:23:59 +0600

    hackerRank sayit

--- array.c
+++ array.c
@@ -1,19 +1,12 @@
 #include<stdio.h>
 int main()
 {
-   int n;
-   scanf("%d",&n);
-   int a[n];
-
-   for(int i=0; i<n; i++){
-    scanf("%d",&a[i]);
+   int N;
+   scanf("%d", &N);
+   for(int i=1; i<=N; i++){
+    printf("%d. I Want More Assignments\n", i);
    }
-   int sum = 0;
-   for(int i=0; i<n; i++){
-    sum += a[i];
-    
-   }
-   printf("%d", sum);
+   
 
 return 0;
 }
